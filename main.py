a98db4ec2df3585bce06c281f9b158205dd9e693
ViKTerS <wireless54544@gmail.com>
2024-10-17 22:28:05 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import discord
+from discord.ext import commands
+
+
+from myserver import server_on
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print("Bot Online!")
+    print("555")
+    synced = await bot.tree.sync()
+    print(f"{len(synced)} command(s)")
+
+
+server_on()
+
+bot.run(os.getenv('TOKEN'))
